39bd23854e683a3807669c906f73fec5b023e1c0
musicmark <139861925+musicmark@users.noreply.github.com>
2025-04-22 13:39:27 +0900

    Create startCommandCog.py

---
+++ Tamplate1/cogs/startCommandCog.py
@@ -0,0 +1,12 @@
+from discord.ext import commands
+
+class Commands(commands.Cog):
+    def __init__(self, bot):
+        self.bot = bot
+
+    async def main(self, ctx):
+      pass
+
+async def setup(bot: commands.Bot):
+    await bot.add_cog(Commands(bot))
+    print(f"Cog {__name__} is now available.")
